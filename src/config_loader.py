b64c1dd143d8e3083071c55ed8db96e1777329f5
Katelyn Gigante <clockwork.singularity@gmail.com>
2024-05-28 22:11:26 +1000

    Config file improvements

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,7 +1,13 @@
+import os
+
 from yaml import safe_load
 
 def load_config(*keys: str):
-    with open('config.yaml', 'r', encoding='utf-8') as f:
+    fn = 'config.yaml'
+    if not os.path.exists(fn):
+        fn = 'config.example.yaml'
+        
+    with open(fn, 'r', encoding='utf-8') as f:
         data: dict = safe_load(f)
         
     for key in keys:
